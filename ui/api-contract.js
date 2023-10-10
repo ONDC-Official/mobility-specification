93e2f239dc09a82049b238a4e56301ec0e5143a9
Anuj Sharma <anuj.sharma@ondc.org>
2023-10-10 13:35:49 +0530

    fix: build issue

--- ui/api-contract.js
+++ ui/api-contract.js
@@ -18,19 +18,23 @@ async function readBuildFile(branchName) {
       },
     });
     const formattedResponse = await response?.json();
-    // if(formattedResponse?.download_url){
-    //   const rawResponse = await fetch(formattedResponse.download_url, {
-    //     // headers: {
-    //     //   Authorization: "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",
-    //     // },
-    //   });
-    //   const formattedrawResponse = await rawResponse?.text();
-    //   build_spec = JSON.parse(getStringAfterEquals(formattedrawResponse));
-    //   onFirstLoad(build_spec);
-    // }
-    let splitedText = atob(formattedResponse?.content);
-    build_spec = JSON.parse(getStringAfterEquals(splitedText));
-    onFirstLoad(build_spec);
+    // reading data using github raw apis.
+    if(formattedResponse?.download_url){
+      setTimeout(async ()=>{
+        const rawResponse = await fetch(formattedResponse.download_url, {
+          // headers: {
+          //   Authorization: "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",
+          // },
+        });
+        const formattedrawResponse = await rawResponse?.text();
+        build_spec = JSON.parse(getStringAfterEquals(formattedrawResponse));
+        onFirstLoad(build_spec);
+      },1200)
+    }
+
+    // let splitedText = atob(formattedResponse?.content);
+    // build_spec = JSON.parse(getStringAfterEquals(splitedText));
+    // onFirstLoad(build_spec);
     
   } catch (error) {
     console.log("Error fetching contract", error?.message || error);
