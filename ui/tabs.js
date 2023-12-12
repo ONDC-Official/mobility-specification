abddfc5178fa3d145a54ed0e47b19dbe23e55c57
Sourabh Saini <sourabhsaini@WILASSETA0181.local>
2023-12-12 13:39:25 +0530

    feat:error codes & cancellation reason

--- ui/tabs.js
+++ ui/tabs.js
@@ -9,6 +9,7 @@ function onFirstLoad(build_spec){
       //addExample("on-demand")
       loadFlows(data["x-flows"])
       loadAttributes(data["x-attributes"])
+      loadErrors(data["x-errorcodes"])
 }
 
 window.onload = function(){
