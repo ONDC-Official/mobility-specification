ce9b20d05548ca984482daddc44d5d2ebe39c691
Anuj Sharma <anuj.sharma@ondc.org>
2023-12-12 15:06:21 +0530

    feat: updated only with metro usecase

---
+++ api/components/examples/Untitled-1..js
@@ -0,0 +1,29 @@
+[
+    {
+              "cancel_by":{
+                "range":{
+                    "end":"2023-08-23T10:29:00.065"
+                    }
+                },
+                "duration":"PT1H"
+            },
+              "reason_required": true,
+              "cancellation_fee": {
+                "percentage": "0"
+              }
+            },
+    {
+              "cancel_by":{
+                "range":{
+                    "end": "2023-08-23T10:35:00.065"
+                },
+                "duration":{
+                  PT1H
+                }
+        }
+              "reason_required": true,
+              "cancellation_fee": {
+                "percentage": "100"
+              }
+            },  
+  ]
