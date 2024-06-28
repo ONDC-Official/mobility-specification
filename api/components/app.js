20a9124a02b29f823f557873072827805c38d012
Anuj Sharma <anuj.sharma@ondc.org>
2024-06-28 20:15:26 +0530

    feat: added event ticketing

--- api/components/app.js
+++ api/components/app.js
@@ -314,17 +314,17 @@ async function getSwaggerYaml(example_set, outputPath) {
       hasTrueResult = await validateTags(tags, schemaMap);
     }
 
-    if (!process.argv.includes(SKIP_VALIDATION.attributes) && !hasTrueResult) {
-      hasTrueResult = await validateAttributes(attributes, schemaMap);
-    }
+    // if (!process.argv.includes(SKIP_VALIDATION.attributes) && !hasTrueResult) {
+    //   hasTrueResult = await validateAttributes(attributes, schemaMap);
+    // }
 
-    if (!process.argv.includes(SKIP_VALIDATION.exampleAttributes) && !hasTrueResult) {
-      await validateExamplesAttributes(exampleSets, attributes)
-    }
+    // if (!process.argv.includes(SKIP_VALIDATION.exampleAttributes) && !hasTrueResult) {
+    //   await validateExamplesAttributes(exampleSets, attributes)
+    // }
 
-    if (process.argv.includes(BUILD.checkAttributes) && !hasTrueResult) {
-        await checkAttributes(exampleSets, attributes)
-    }
+    // if (process.argv.includes(BUILD.checkAttributes) && !hasTrueResult) {
+    //     await checkAttributes(exampleSets, attributes)
+    // }
     if (hasTrueResult) return;
 
     if (!hasTrueResult) {
@@ -564,12 +564,12 @@ function addEnumTag(base, layer) {
   base["x-tags"] = layer["tags"];
   base["x-flows"] = layer["flows"];
   base["x-examples"] = layer["examples"];
-  base["x-attributes"] = layer["attributes"];
-  base["x-errorcodes"] = layer["error_codes"];
+  // base["x-attributes"] = layer["attributes"];
+  // base["x-errorcodes"] = layer["error_codes"];
   base["x-tlc"] = layer["tlc"];
-  base["x-featureui"] = layer["feature-ui"]
-  base["x-sandboxui"] = layer["sandbox-ui"]
-  base["x-testcasesui"] = layer["testcases-ui"]
+  // base["x-featureui"] = layer["feature-ui"]
+  // base["x-sandboxui"] = layer["sandbox-ui"]
+  // base["x-testcasesui"] = layer["testcases-ui"]
 
 }
 
